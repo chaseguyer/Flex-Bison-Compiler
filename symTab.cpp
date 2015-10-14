2384e471434ab81c98fd6522a14951ca377df30f
chaseguyer <chaseguyer@gmail.com>
2015-10-14 13:51:16 -0700

    Finished symbol table and started errors

---
+++ symTab.cpp
@@ -0,0 +1,110 @@
+#include "symTab.h"
+
+using namespace std;
+
+/*
+int counter;
+void pointerPrintAddr(void *data) {
+	printf("0x%016llx ", (unsigned long long int)(data));
+}
+
+void pointerPrintStr(void *data) {
+	printf("%s ", (char*)(data));
+}
+*/
+
+
+// Class Scope
+Scope::Scope(string newname) {
+	name = newname;
+}
+
+Scope::~Scope() {
+}
+
+void Scope::print(void (*printData)(void *)) {
+	printf("Scope: %-15s -----------------\n", name.c_str());
+	for(map<string, void *>::iterator it = symbols.begin(); it != symbols.end(); it++) {
+		printf("%20s: ", (it->first).c_str());
+		printData(it->second);
+		printf("\n");		
+	}
+}
+
+void Scope::applyToAll(void (*action)(string, void *)) {
+	for(map<string, void *>::iterator it = symbols.begin(); it != symbols.end(); it++) {
+		action(it->first, it->second);
+	}
+}
+
+bool Scope::insert(string sym, void *ptr) {
+	if(symbols.find(sym) == symbols.end()) {
+		symbols[sym] = ptr;
+		return true;
+	} else 
+		return false;
+}
+
+void *Scope::lookup(string sym) {
+	if(symbols.find(sym) != symbols.end()) {
+		return symbols[sym];
+	} else
+		return NULL;
+}
+
+// Class SymbolTable
+SymbolTable::SymbolTable() {
+	enter((string)"Global");
+}
+
+int SymbolTable::depth() {
+	return stack.size();
+}
+
+void SymbolTable::print(void (*printData)(void *)) {
+    printf("===========  Symbol Table  ===========\n");
+	for(vector<Scope *>::iterator it = stack.begin(); it != stack.end(); it++) {
+		(*it)->print(printData);
+	}
+    printf("===========  ============  ===========\n");
+}
+
+void SymbolTable::applyToAllGlobal(void (*action)(string, void *)) {
+	stack[0]->applyToAll(action);
+}
+
+void SymbolTable::enter(string name) {
+	stack.push_back(new Scope(name));
+}
+
+void SymbolTable::leave() {
+	if(stack.size() > 1) {
+		delete stack.back();
+		stack.pop_back();
+	} else 
+        printf("ERROR(SymbolTable): You cannot leave global scope.  Number of scopes: %d.\n", (int)stack.size());
+}
+
+void * SymbolTable::lookup(string sym) {
+	void *data;
+	for(vector<Scope *>::reverse_iterator it = stack.rbegin(); it != stack.rend(); it++) {
+		data = (*it)->lookup(sym);
+		if(data != NULL)
+			break;
+	}
+	return data;
+}
+
+void * SymbolTable::lookupGlobal(string sym) {
+	void *data;
+	data = stack[0]->lookup(sym);
+	return data;
+}
+
+bool SymbolTable::insert(string sym, void *ptr) {
+	return (stack.back())->insert(sym, ptr);
+}
+
+bool SymbolTable::insertGlobal(string sym, void *ptr) {
+	return stack[0]->insert(sym, ptr);
+}	 
---
+++ symTab.h
@@ -0,0 +1,49 @@
+#ifndef _SYMTAB_H_
+#define _SYMTAB_H_
+
+#include<map>
+#include<vector>
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+
+using namespace std;
+
+class Scope {
+private:
+	string name;
+	map<string, void *> symbols;
+
+public:
+	Scope(string newname);
+	~Scope();
+	void print(void (*printData)(void *));
+	void applyToAll(void (*action)(string, void *));
+	bool insert(string sym, void *ptr);
+	
+	void *lookup(string sym);
+
+	string scopeName() { return name; };
+};
+
+class SymbolTable {
+private:
+	vector<Scope *> stack;
+	
+public:
+	SymbolTable();
+	int depth();
+	void print(void (*printData)(void *));
+	void applyToAllGlobal(void (*action)(string, void *));
+	void enter(string name);
+	void leave();
+	void *lookup(string sym);
+
+	void *lookupGlobal(string sym);
+
+	bool insert(string sym, void *ptr);
+
+	bool insertGlobal(string sym, void *ptr);
+};
+
+#endif
